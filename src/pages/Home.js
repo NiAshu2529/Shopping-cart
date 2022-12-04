175c80c2a0798060091857d9ba836efdf97f4ee3
ASHUTOHS_JHA <bhardwajnidhi04@gmail.com>
2022-12-04 17:19:20 +0530

    changes in form.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Product from '../components/Product'
 const Home = () => {
   return (
     <>
-    <h2 className="heading">Welcome to the Redux toolkit store</h2><section>
+    <h2 className="heading">Welcome to my store </h2><section>
           <h3>Products</h3>
           <Product />
       </section>
